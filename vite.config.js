967b9aa41d6625e4afbedf000607cd58763deb2e
MichielKoster <michielk@hotmail.com>
2026-06-17 09:54:47 +0200

    Add files via upload

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist',
-    chunkSizeWarningLimit: 2000,
-  }
+  build: { outDir: 'dist', chunkSizeWarningLimit: 2000 }
 })
